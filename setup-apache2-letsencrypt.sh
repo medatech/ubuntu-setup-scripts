b8ccf55db0605f56ab23b91dadaff1f67253688b
Martin Adams <martin@meda.co.uk>
2016-04-10 21:54:54 +0100

    Create setup-apache2-letsencrypt.sh

---
+++ setup-apache2-letsencrypt.sh
@@ -0,0 +1,11 @@
+sudo apt-get install -y apache2
+
+sudo a2enmod proxy_http
+
+sudo service apache2 restart
+
+cd
+git clone https://github.com/letsencrypt/letsencrypt
+cd letsencrypt
+./letsencrypt-auto --help
+./letsencrypt-auto --apache -d meda.io
