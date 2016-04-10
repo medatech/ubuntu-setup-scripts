aa8dcfb5ad0e053e81b6be212837e6c8a1367fd7
Martin Adams <martin@meda.co.uk>
2016-04-10 22:35:29 +0100

    Adding automatic install of apt-get packages

--- setup-jekyll.sh
+++ setup-jekyll.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
