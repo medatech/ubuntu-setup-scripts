d64ccc83a952459d14b20f22146f93b5f576a515
Martin Adams <martin@meda.co.uk>
2016-04-10 22:00:50 +0100

    Adding prompt

--- setup-apache2-letsencrypt.sh
+++ setup-apache2-letsencrypt.sh
@@ -1,3 +1,6 @@
+echo "What domain do you want to make HTTPS?"
+read domain
+
 sudo apt-get install -y apache2
 
 sudo a2enmod proxy_http
@@ -8,4 +11,4 @@ cd
 git clone https://github.com/letsencrypt/letsencrypt
 cd letsencrypt
 ./letsencrypt-auto --help
-./letsencrypt-auto --apache -d meda.io
+./letsencrypt-auto --apache -d $domain
