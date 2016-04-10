473a10b6ed72f8d770cae9c3f411ead63e307025
Martin Adams <martin@meda.co.uk>
2016-04-10 21:56:43 +0100

    Prompt for username

--- setup-ubuntu-user.sh
+++ setup-ubuntu-user.sh
@@ -1,7 +1,10 @@
+echo "What username would you like to configure?"
+read username
+
 # Create user ubuntu and add to sudo list
-sudo useradd -d /home/ubuntu -m ubuntu
-sudo passwd ubuntu
-sudo usermod -a -G sudo ubuntu
+sudo useradd -d /home/$username -m $username
+sudo passwd $username
+sudo usermod -a -G sudo $username
 
 # Use Bash by default
-sudo usermod -s /bin/bash ubuntu
+sudo usermod -s /bin/bash $username
