71df0e8632d35a4f69cca4b3b57d8622d741293e
Martin Adams <martin@meda.co.uk>
2016-04-10 22:00:02 +0100

    Adding prompts for usernames and password

--- setup-cloud9.sh
+++ setup-cloud9.sh
@@ -1,3 +1,12 @@
+echo "What user is this being install under?"
+read username
+
+echo "What cloud9 username would you like to use?"
+read c9username
+
+echo "What cloud9 password would you like to use?"
+read -s c9password
+
 echo "Installing Node"
 
 sudo apt-get update
@@ -13,6 +22,6 @@ cd c9sdk
 sudo scripts/install-sdk.sh
 
 echo "Running Cloud9 on reboot"
-echo "cd c9sdk && nohup node server.js --a ubuntu:ubuntu -w /home/ubuntu/projects/ &" > /home/ubuntu/startCloud9.sh
-chmod +x /home/ubuntu/startCloud9.sh
-{ crontab -l ; echo -e "\n@reboot /home/ubuntu/startCloud9.sh" ; } | crontab
+echo "cd c9sdk && nohup node server.js --a $c9username:$c9password -w /home/$username/projects/ &" > /home/$username/startCloud9.sh
+chmod +x /home/$username/startCloud9.sh
+{ crontab -l ; echo -e "\n@reboot /home/$username/startCloud9.sh" ; } | crontab
